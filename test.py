1c039aad3cea066074a8834c851e5c93fa08f4fd
cjorasia <59288893+Cjorasia@users.noreply.github.com>
2020-05-27 15:08:05 +0530

    added comment

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+# comment
 dataset = pd.read_csv('Dataset/SalaryData.csv')
 
 # Checking Dataset
